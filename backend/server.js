4c526407bf223dc2b0b3dd043cf9edb00fc216cf
Taha Khan <mohdtahakhan13@gmail.com>
2025-04-12 12:31:29 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,15 @@ const adminOrderRoutes=require("./routes/adminOrderRoutes")
 
 const app=express();
 app.use(express.json())
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: [
+      'https://rabbit-fcer.vercel.app',
+      'http://localhost:9000' // For local development
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
 
 
 dotenv.config();
